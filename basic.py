7960d047f6fbc6b281ac1e466beb1c77bff46b44
Megha706765 <meghathakur2003123@gmail.com>
2026-02-27 11:46:02 +0530

    Mathformula

---
+++ basic.py
@@ -0,0 +1,105 @@
+print("hello word")
+
+num =7
+num2 =num
+
+print(id(num))
+print(id(num2))
+
+num3 = 3
+
+print(5**2)
+print(5//2)
+print(5/2)
+print(5%2)
+
+v1 = 8
+v2 = 7
+print(v1+v2)
+print(v1-v2)
+print(v1++v2)
+print(v1--v2)
+print(v1+-v2)
+print(v1*v2)
+print(v1/v2)
+print(v1//v2)
+print(v1%v2)
+
+
+li = [5,6,7,8,"hello",89.0,True]
+print(li[5:7])
+print(li[-2:-1])
+print(li[6:800])
+print(li[-2:])
+print(li[-4:])
+
+
+# 2D
+li = [[56,55],[43,45],[87,98]]
+print(len(li))
+print(li[0])
+print(li[[0][0]])
+
+
+# 3D
+li = [[56,55],[43,[45,64]],[87,98]]
+
+
+# to use the for loop
+
+
+li = [5,6,7,8,"hello",89.0,True]
+for num in li:
+    print(num)
+    print("Bye..")
+
+
+
+numbers = [6,9,7,4,8,9,3,10]
+for n in numbers:
+    print(n*n)
+
+#     #or
+
+#     print(n**3)
+#     print(n+5)
+
+
+# twodlist =[[2,3],[6,5],[8,9],[4,7]]
+# #print(len(twodlist))
+
+# for i in twodlist:
+    # print(i)
+#     print(i[0])
+#     print(i[-1])
+# print(i,i[-1])
+
+
+
+#math function
+
+# import math
+
+# print(math.sqrt(3))
+# print(math.sqrt(16))
+
+
+
+# import cv2
+# imge = cv2.imread("pujya.jpg")
+# cv2.imshow("this is my file",imge)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+print("hello")
+
+
+
+
+
+
+    
+
+
+
+
